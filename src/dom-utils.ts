e02c4d92aac9c189f34b63094932fae701b2df1b
josef <info@slezak-it.de>
2023-03-31 15:39:38 +0200

    zwischenstand migration

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -1,20 +1,14 @@
 import { calculateRate } from "./formula";
-
 const days = document.querySelector("#days") as HTMLInputElement;
 const growthrate = document.querySelector("#growthrate") as HTMLInputElement;
 const start = document.querySelector("#start") as HTMLInputElement;
 const result = document.querySelector("#result") as HTMLSpanElement;
 const daysView = document.querySelector("#daysView");
 
+days.addEventListener("input", updateDaysView);
+
 function updateDaysView() {
   daysView!.innerHTML = days.value;
 }
 
-function addInputListeners() {
-  days.addEventListener("input", calculateRate);
-  growthrate.addEventListener("input", calculateRate);
-  start.addEventListener("input", calculateRate);
-  days.addEventListener("input", updateDaysView);
-}
-
-export { days, growthrate, start, result, addInputListeners };
+export { days, growthrate, start, result };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import { addInputListeners, days, growthrate, start } from "./dom-utils";
+import { days, growthrate, start } from "./dom-utils";
 import { calculateRate } from "./formula";
 
 growthrate.value = "1.1";
 start.value = "1";
 days.value = "1";
 
+function addInputListeners() {
+  days.addEventListener("input", calculateRate);
+  growthrate.addEventListener("input", calculateRate);
+  start.addEventListener("input", calculateRate);
+}
+
 function initApp() {
   addInputListeners();
   calculateRate();
 }
 initApp();
+function updateDaysView(this: HTMLInputElement, ev: Event) {
+  throw new Error("Function not implemented.");
+}
